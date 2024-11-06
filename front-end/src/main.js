6b878a2d43c1f41dcb78be2e53399f33df38088d
WangJiying <w2084151024@gmail.com>
2024-11-06 11:14:34 +0800

    创建项目

---
+++ front-end/src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import Antd from 'ant-design-vue';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'ant-design-vue/dist/reset.css';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router).use(ElementPlus).use(Antd)
+
+app.mount('#app')
